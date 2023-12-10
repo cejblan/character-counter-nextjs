630d8fda4f8e18f692ddd4fad6c599f64ea6ab76
benfran21 <benfran21@hotmail.com>
2023-12-10 10:33:51 -0400

    Se agrego googleAds

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6341118636252822"
+          crossorigin="anonymous"></script>
+      </head>
       <body className={"text-center " + inter.className}>
         <Navbar />
         {children}
