695605b739210dc433214c0935a6a6cde7071a85
benfran21 <benfran21@hotmail.com>
2023-12-10 12:06:20 -0400

    Se agrego la pagina privacy al footer

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,10 @@
+import Link from "next/link"
+
 export default function footer() {
     return (
         <footer className="bg-blue-600 text-center text-lg w-full text-white p-2">
             <p>Contador de Caracteres con Next.js, React y TailwindCSS - © Copyright 2024 - Diseño Cejblan</p>
+            <Link className="font-extrabold" href="/privacy">Politicas de privacidad</Link>
         </footer>
     )
 }
