8cd8f4c551a7ae87be63d5e4d23b9bbf3dd7d337
benfran21 <benfran21@hotmail.com>
2023-12-11 11:01:43 -0400

    Se le dio formato a la pagina pribacy

--- app/privacy/page.js
+++ app/privacy/page.js
@@ -1,55 +1,59 @@
 export default function privacy() {
     return (
-        <section className="privacy">
-            <h1>Título: Política de privacidad</h1>
-            <h2>Introducción:</h2>
+        <section className="privacy text-justify py-4 px-8">
+            <h1 className="text-3xl text-center font-bold py-3">Política de Privacidad</h1>
             <p>En esta política de privacidad, explicamos cómo recopilamos, utilizamos y compartimos su información personal. Esta política se aplica a todos los usuarios de nuestro sitio web y aplicación.</p>
             <h2>Recopilación de datos:</h2>
-            <p>Podemos recopilar información personal sobre usted de varias maneras, incluyendo:</p>
-            <p>Información que usted nos proporciona: Podemos recopilar información personal que usted nos proporciona voluntariamente, como su nombre, dirección de correo electrónico, dirección postal, número de teléfono, información de pago y otra información de contacto.
-                Información que recopilamos automáticamente: Podemos recopilar información sobre usted automáticamente cuando utiliza nuestro sitio web o aplicación, como su dirección IP, información del navegador, información del dispositivo y otra información de uso.</p>
-                <p>    
-                Información de terceros: Podemos recopilar información sobre usted de terceros, como socios comerciales, proveedores de servicios y otras fuentes.
-                Uso de datos:
-
-                Podemos utilizar su información personal para los siguientes fines:
-
-                Para proporcionarle nuestros servicios: Podemos utilizar su información personal para proporcionarle nuestros servicios, como permitirle crear una cuenta, realizar una compra, ponerse en contacto con nosotros y utilizar otras funciones de nuestro sitio web o aplicación.
-                Para mejorar nuestros servicios: Podemos utilizar su información personal para mejorar nuestros servicios, como personalizar su experiencia, analizar el uso de nuestro sitio web o aplicación y desarrollar nuevos productos y servicios.
-                Para fines de marketing: Podemos utilizar su información personal para enviarle comunicaciones de marketing, como boletines informativos, ofertas especiales y otra información sobre nuestros productos y servicios.
-                Para cumplir con la ley: Podemos utilizar su información personal para cumplir con las leyes aplicables, como las leyes fiscales, las leyes de protección de datos y las leyes de seguridad nacional.
-                Compartición de datos:
-
-                Podemos compartir su información personal con las siguientes personas:
-
-                Nuestros empleados y contratistas: Podemos compartir su información personal con nuestros empleados y contratistas que necesitan acceder a ella para proporcionarle nuestros servicios.
-                Terceros proveedores de servicios: Podemos compartir su información personal con terceros proveedores de servicios que nos ayudan a proporcionar nuestros servicios, como proveedores de alojamiento web, proveedores de análisis y proveedores de marketing.
-                Otras partes: Podemos compartir su información personal con otras partes en las siguientes circunstancias:
-                Con su consentimiento.
-                Para cumplir con la ley.
-                Para proteger nuestros derechos o la seguridad de otros.
-                Sus derechos:
-
-                Tiene los siguientes derechos en relación con su información personal:
-
-                Derecho de acceso: Tiene derecho a acceder a su información personal que tenemos.
-                Derecho de rectificación: Tiene derecho a solicitar que corrijamos cualquier información personal inexacta o incompleta que tengamos sobre usted.
-                Derecho de eliminación: Tiene derecho a solicitar que eliminemos su información personal.
-                Derecho de restricción del tratamiento: Tiene derecho a solicitar que restrinjamos el tratamiento de su información personal.
-                Derecho a la portabilidad de los datos: Tiene derecho a solicitar que le proporcionemos una copia de su información personal en un formato estructurado, de uso común y legible por máquina.
-                Derecho a oponerse al tratamiento: Tiene derecho a oponerse al tratamiento de su información personal por motivos relacionados con su situación particular.
-                Para ejercer estos derechos, puede ponerse en contacto con nosotros utilizando los datos de contacto que se proporcionan a continuación.
-
-                Seguridad de los datos:
-
-                Tomamos medidas de seguridad para proteger su información personal contra el acceso, la divulgación, la alteración o la destrucción no autorizados. Estas medidas incluyen:
-
-                Seguridad física: Mantenemos nuestras instalaciones físicas seguras y protegidas.
-                Seguridad técnica: Utilizamos medidas de seguridad técnicas, como el cifrado, para proteger su información personal.
-                Seguridad administrativa: Tenemos políticas y procedimientos administrativos para proteger su información personal.
-                Cambios en esta política:
-
-                Podemos cambiar esta política de privacidad de vez en cuando. Si hacemos algún cambio importante, le notificaremos por correo electrónico o mediante un aviso en nuestro sitio web.</p>
-        </section>
+            <ul>
+                <h3>Podemos recopilar información personal sobre usted de varias maneras, incluyendo:</h3>
+                <li>Información que usted nos proporciona: Podemos recopilar información personal que usted nos proporciona voluntariamente, como su nombre, dirección de correo electrónico, dirección postal, número de teléfono, información de pago y otra información de contacto.</li>
+                <li>Información que recopilamos automáticamente: Podemos recopilar información sobre usted automáticamente cuando utiliza nuestro sitio web o aplicación, como su dirección IP, información del navegador, información del dispositivo y otra información de uso.</li>
+            </ul>
+            <h2>Información de terceros:</h2>
+            <p>Podemos recopilar información sobre usted de terceros, como socios comerciales, proveedores de servicios y otras fuentes.</p>
+            <h2>Uso de datos:</h2>
+            <ul>
+                <h3>Podemos utilizar su información personal para los siguientes fines:</h3>
+                <li>Para proporcionarle nuestros servicios: Podemos utilizar su información personal para proporcionarle nuestros servicios, como permitirle crear una cuenta, realizar una compra, ponerse en contacto con nosotros y utilizar otras funciones de nuestro sitio web o aplicación.</li>
+                <li>Para mejorar nuestros servicios: Podemos utilizar su información personal para mejorar nuestros servicios, como personalizar su experiencia, analizar el uso de nuestro sitio web o aplicación y desarrollar nuevos productos y servicios.</li>
+                <li>Para fines de marketing: Podemos utilizar su información personal para enviarle comunicaciones de marketing, como boletines informativos, ofertas especiales y otra información sobre nuestros productos y servicios.</li>
+                <li>Para cumplir con la ley: Podemos utilizar su información personal para cumplir con las leyes aplicables, como las leyes fiscales, las leyes de protección de datos y las leyes de seguridad nacional.</li>
+            </ul>
+            <h2>Compartición de datos:</h2>
+            <ul>
+                <h3>Podemos compartir su información personal con las siguientes personas:</h3>
+                <li>Nuestros empleados y contratistas: Podemos compartir su información personal con nuestros empleados y contratistas que necesitan acceder a ella para proporcionarle nuestros servicios.</li>
+                <li>Terceros proveedores de servicios: Podemos compartir su información personal con terceros proveedores de servicios que nos ayudan a proporcionar nuestros servicios, como proveedores de alojamiento web, proveedores de análisis y proveedores de marketing.</li>
+                <li>
+                    <ol>
+                        <h3>Otras partes: Podemos compartir su información personal con otras partes en las siguientes circunstancias:</h3>
+                        <li>Con su consentimiento.</li>
+                        <li>Para cumplir con la ley.</li>
+                        <li>Para proteger nuestros derechos o la seguridad de otros.</li>
+                    </ol>
+                </li>
+            </ul>
+            <h2>Sus derechos:</h2>
+            <ul>
+                <h3>Tiene los siguientes derechos en relación con su información personal:</h3>
+                <li>Derecho de acceso: Tiene derecho a acceder a su información personal que tenemos.</li>
+                <li>Derecho de rectificación: Tiene derecho a solicitar que corrijamos cualquier información personal inexacta o incompleta que tengamos sobre usted.</li>
+                <li>Derecho de eliminación: Tiene derecho a solicitar que eliminemos su información personal.</li>
+                <li>Derecho de restricción del tratamiento: Tiene derecho a solicitar que restrinjamos el tratamiento de su información personal.</li>
+                <li> Derecho a la portabilidad de los datos: Tiene derecho a solicitar que le proporcionemos una copia de su información personal en un formato estructurado, de uso común y legible por máquina.</li>
+                <li>Derecho a oponerse al tratamiento: Tiene derecho a oponerse al tratamiento de su información personal por motivos relacionados con su situación particular.</li>
+                <li>Para ejercer estos derechos, puede ponerse en contacto con nosotros utilizando los datos de contacto que se proporcionan a continuación.</li>
+            </ul>
+            <h2>Seguridad de los datos:</h2>
+            <ul>
+                <p>Tomamos medidas de seguridad para proteger su información personal contra el acceso, la divulgación, la alteración o la destrucción no autorizados.</p>
+                <h3>Estas medidas incluyen:</h3>
+                <li>Seguridad física: Mantenemos nuestras instalaciones físicas seguras y protegidas.</li>
+                <li> Seguridad técnica: Utilizamos medidas de seguridad técnicas, como el cifrado, para proteger su información personal.</li>
+                <li>Seguridad administrativa: Tenemos políticas y procedimientos administrativos para proteger su información personal.</li>
+            </ul>
+            <h2>Cambios en esta política:</h2>
+            <p>Podemos cambiar esta política de privacidad de vez en cuando. Si hacemos algún cambio importante, le notificaremos por correo electrónico o mediante un aviso en nuestro sitio web.</p>
+        </section >
     )
 }
